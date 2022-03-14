3d7d9edbe0400137ce8e88a5c95e8aac5df8be72
Tyler Hum <tyler.hum@gmail.ca>
2022-03-14 11:04:09 -0600

    changes to index

--- CSRazorSolution/WebApp/Pages/Index.cshtml.cs
+++ CSRazorSolution/WebApp/Pages/Index.cshtml.cs
@@ -1,18 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+#region Additional Namespaces
+using WestWindSystem.BLL; // this is where the services were coded
+using WestWindSystem.Entities; // this is where the entity definition is coded
+#endregion
+
+
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
     {
+        #region Private Service Fields and Class Constructor
         private readonly ILogger<IndexModel> _logger;
+        private readonly BuildVersionServices _buildVersionServices;
 
-        public IndexModel(ILogger<IndexModel> logger)
+/*
+        Services that are registerd using AddTransient<>() can be accessed on the constructor of the webe page class (PageModel)
+        This is refered to as Dependency Injection
+        Each registered service that is injected is listed on the constructor as a parameter
+        ILogger is a service from Microsoft Logging extensions
+
+        We need to add our required service(s) to this page. Our services will be known by the BLL class name.
+
+        When you add a service to the page, you will save the service reference in a private readonly field.
+
+        This variable will be available to all methods within this class
+ */
+
+        public IndexModel(ILogger<IndexModel> logger, BuildVersionServices buildversionservices)
         {
             _logger = logger;
+            _buildVersionServices = buildversionservices;
         }
 
+        
+        #endregion
+
         public string MyName { get; set; }
+
+        public BuildVersion buildVersionInfo { get; set; }
 
         public void OnGet()
         {
@@ -20,12 +47,15 @@ namespace WebApp.Pages
             int value = random.Next(0, 100);
             if (value % 2 == 0)
             {
-                MyName = $"Don welcomes you to the Razor World ({value})";
+                MyName = $"High King Turgon welcomes you to the Razor World ({value})";
             }
             else
             {
                 MyName = null;
             }
+
+            // consume a service (aka method) from the services BuildVersionServices
+            buildVersionInfo = _buildVersionServices.GetBuildVersion();
         }
     }
 }
