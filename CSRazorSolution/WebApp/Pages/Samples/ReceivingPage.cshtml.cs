568d4363bd17b25ce2558039bb7350239b67971b
Tyler Hum <tyler.hum@gmail.ca>
2022-03-29 09:32:29 -0600

    Adds notes

---
+++ CSRazorSolution/WebApp/Pages/Samples/ReceivingPage.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Samples
+{
+    public class ReceivingPageModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+
+        public int? territoryid { get; set; }
+        public void OnGet()
+        {
+
+        }
+    }
+}
